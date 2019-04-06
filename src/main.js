7918d7b7f015f8c50b1d1ada8182fa77933636d5
Kateryna Vitruk <katherina.vitruk@gmail.com>
2019-04-06 18:33:44 +0300

    add utils mixin

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import App from './App.vue';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import utils from './mixins/utils';
+
 Vue.config.productionTip = false;
+
+Vue.mixin(utils);
 
 new Vue({
   render: h => h(App),
