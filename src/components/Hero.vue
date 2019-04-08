88ea13e668aff2fd38640447fd478933b569964c
Kateryna Vitruk <katherina.vitruk@gmail.com>
2019-04-08 12:37:43 +0300

    update Hero - add form

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,47 @@
 <template>
-    <div></div>
+    <div class="hero">
+        <form class="form">
+            <div class="form-group">
+                <input class="form-control" placeholder="Enter your word" v-model="word">
+                <button type="button" class="btn btn-primary">Change!</button>
+            </div>
+        </form>
+        <canvas id="hero-canvas"></canvas>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'Hero'
+  name: 'Hero',
+  data() {
+    return {
+      word: 'hello',
+      title: ''
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+    .form {
+        position: absolute;
+        bottom: 100px;
+        left: calc(50% - 150px);
+        z-index: 100;
+
+        input {
+            width: 300px;
+            height: 50px;
+            border: 1px solid $grey;
+            background-color: #252525;
+            margin-bottom: 20px;
+            color: $white;
+        }
+
+        button {
+            width: 300px;
+            height: 50px;
+            background: linear-gradient(100.24deg, #3840FF 0%, rgba(26, 31, 145, 0.58) 100%);
+        }
+    }
 </style>
