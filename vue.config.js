caa6e730a34e8a877750223f5e42d22dae1e8948
Kateryna Vitruk <katherina.vitruk@gmail.com>
2019-04-08 12:34:04 +0300

    add sass-resource-loader

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,18 @@ module.exports = {
         THREE: 'three'
       })
     ]
+  },
+  chainWebpack: config => {
+    const oneOfsMap = config.module.rule('scss').oneOfs.store;
+
+    oneOfsMap.forEach(item => {
+      item
+        .use('sass-resources-loader')
+        .loader('sass-resources-loader')
+        .options({
+          resources: 'src/assets/scss/_base.scss'
+        })
+        .end()
+    })
   }
 };
